786ff6d1fdfcce6da0efca382c769c8d3ba0673d
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-03-09 16:03:37 -0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,34 @@
- #include <iostream>
+#include <iostream>
+#include <vector>
 
-int main(){
 
+using namespace std;
+
+class PlayerInfo{
+    public:
+        string name;
+        char obj;
+        int victories;
+        PlayerInfo(string _name, char obj, int victories){
+            name = _name;
+            obj = obj;
+            victories = victories;
+        }
+        virtual string getName(){
+            return name;
+        }
+};
+
+int main() {
+  vector <PlayerInfo*> Player;
+
+
+  PlayerInfo *auxinfos = new PlayerInfo("Computador", 'X', 0);
+  Player.push_back(auxinfos);
+  auxinfos = new PlayerInfo("Nome1", 'O', 0);
+  Player.push_back(auxinfos);
+
+  printf("%s", Player[0]->getName());
+
+  return 0x29A;
 }
